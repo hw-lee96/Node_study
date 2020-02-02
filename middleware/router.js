cffb66fc0aad784f2aa2c04790a7fcb86c941817
이형우 <hyeongu0305@gmail.com>
2020-02-02 18:12:38 +0900

    라우터 수정

--- middleware/router.js
+++ middleware/router.js
@@ -1,8 +1,6 @@
-const fs = require('fs');
 const urlParser = require('url');
 
-// 관리하는 페이지가 많아짐에 따라서 코드의 복잡도가 급격히 높아지게 됩니다. 복잡도를 낮추는 방법이 라우터입니다.  
-// 출처 : 생활코딩
+// 페이지가 많아짐에 따라 코드 복잡도가 높아지며, 복잡도를 낮추기 위해 라우팅을 한다.
 
 module.exports = {
     init : function (req, res) {
@@ -12,24 +10,41 @@ module.exports = {
             return acc+"/"+current;
         });
 
-        var fileName = path[path.length-1] == '' ? 'index' : path[path.length-1];
+        var contPrefix = path[path.length-2] == '' ? 'index' : path[path.length-2];     
+        var funcNm = path[path.length-1] == '' ? 'index' : path[path.length-1];     
+        var GETParam = this.processingJson(urlParser.parse(req.url).query);        // querystring을 json으로 변환
+        
+        console.log(req);
 
         var data = {
-            dir : dir,
-            fileName : fileName,
-            param : urlParser.parse(req.url).query                      //querystring으로 전달된 값
+            dir : dir,                                  // 파일 경로
+            contPrefix : contPrefix,                    // Controller 앞에 붙일 접두사
+            funcNm : funcNm,                            // 호출 할 함수명
+            GETParam : GETParam,                        // querystring으로 전달된 값
+            method : req.method                         // 요청 method 타입
         }
-        
         this.routing(req, res, data);
     },
+
+    processingJson : function (querystring) {
+        if ( querystring == '' || querystring == null ) return '';
+        var param = querystring
+                    .split('&')                                 // '&'로 스플릿
+                    .map(v=>v.split('='))                       // map으로 각 원소를 '='로 스플릿
+                    .map( ([k,...vs]) => [k, vs.join('=')])     //join함수로 key가 아닌 value에 split된 '='을 다시 합쳐줌
+                    .reduce((acc, [k,v]) => {                   // reduce로 key = value형태로 변환
+                        acc[k.trim()] = decodeURIComponent(v);
+                        return acc;
+                    }, {});
+        return param;
+    },
+
     routing : function (req, res, data) {
-        fs.readFile(`./view${data.dir}/${data.fileName}.html`, (err, page) => {
-            if (err) {
-                res.writeHead(404, {'Content-Type' : 'text/html; charset=utf-8'});
-                res.end('페이지가 존재하지 않습니다.');     //404페이지로 변경해야함
-            };
-            //data 넘기는 내용 추가해야함
-            res.end(page);
-        });
+        try {
+            let controller = require(`../view${data.dir}/${data.contPrefix}/${data.contPrefix}Controller`);
+            controller.init(req, res, data);
+        } catch(e) {
+            console.log(e);
+        }
     }
 }
